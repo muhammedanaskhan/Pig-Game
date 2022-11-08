ac811c98fd4ce6e229ad185d68d347daeafce15b
muhammedanaskhan <muhammedanas5186@gmail.com>
2022-11-08 07:23:01 -0800

    button hold implemented

--- script.js
+++ script.js
@@ -15,29 +15,62 @@ const cur0El = document.getElementById('current--0');
 //starting conditions
 
 
-score0El.textContent = 12;
-score1El.textContent = 2;
+score0El.textContent = 0;
+score1El.textContent = 0;
 let curScore= 0;
 let activePlayer = 0;
+let playing = true;
 
 //roll dice
 
 btnRoll.addEventListener('click', function(){
-    let ranNum = Math.trunc(Math.random() * 6)+1;
-    diceEl.style.visibility = 'visible';
-    diceEl.src = `dice-${ranNum}.png`;
 
-    if(ranNum ===1){  //switch player
-        let cur = document.getElementById(`current--${activePlayer}`);
-        cur.textContent=0; 
-        curScore=0;
-       
+    if(playing){
+            let ranNum = Math.trunc(Math.random() * 6)+1;
+        diceEl.style.visibility = 'visible';
+        diceEl.src = `dice-${ranNum}.png`;
+
+        if(ranNum ===1){  //switch player
+            let cur = document.getElementById(`current--${activePlayer}`);
+            cur.textContent=0; 
+            curScore=0;
+        
+            activePlayer = activePlayer === 0 ? 1 : 0;
+            player0El.classList.toggle('player--active');
+            player1El.classList.toggle('player--active');
+        }else{
+            curScore+=ranNum;
+            let cur = document.getElementById(`current--${activePlayer}`);
+            cur.textContent = curScore;    
+        }
+    }
+    
+});
+
+btnHold.addEventListener('click', function(){
+
+    if(playing){
+            let cur = document.getElementById(`current--${activePlayer}`);
+        let currScore = Number(cur.textContent);
+
+        let mainScore = document.getElementById(`score--${activePlayer}`);
+        mainScore.textContent = Number(mainScore.textContent)+currScore;
+
+        cur.textContent = 0;
+
+        if(mainScore >= 100){
+            playing=false;
+            document.querySelector(`player--${activePlayer}}`).classList.add('player--winner');
+            document.querySelector(`player--${activePlayer}}`).classList.remove('player--active');
+        }
+
+        //switch player
         activePlayer = activePlayer === 0 ? 1 : 0;
         player0El.classList.toggle('player--active');
         player1El.classList.toggle('player--active');
-    }else{
-        curScore+=ranNum;
-        let cur = document.getElementById(`current--${activePlayer}`);
-        cur.textContent = curScore;    
+
+        cur = document.getElementById(`current--${activePlayer}`);
+        cur.textContent = 0;
     }
+    
 });
